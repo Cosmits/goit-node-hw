6bcc9310f9dda88f30e6fd10deb287478897e777
Cosmits <salge.cos@gmail.com>
2023-08-30 20:19:13 +0200

    hw-02

--- models/contacts.js
+++ models/contacts.js
@@ -1,16 +1,106 @@
-// const fs = require('fs/promises')
+import fs from 'fs/promises'
+import path from 'path'
+import { v1 as uuidv1 } from 'uuid'
 
-const listContacts = async () => {}
+const contactsPath = path.resolve('models', 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf-8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(err);
+    return null
+  }
 
-const removeContact = async (contactId) => {}
+}
 
-const addContact = async (body) => {}
+const getContactById = async (id) => {
+  try {
+    const allContacts = await listContacts()
+    const foundContact = allContacts?.find(el => el.id === id);
+    return foundContact || null;
+  } catch (error) {
+    console.error(err);
+    return null
+  }
+}
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (id) => {
+  try {
+    const allContacts = await listContacts();
+    const indexEl = allContacts.findIndex(el => el.id === id);
 
-module.exports = {
+    if (indexEl === -1) return null;
+
+    const [result] = allContacts.splice(indexEl, 1);
+    fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return result;
+
+  } catch (error) {
+    console.error(err);
+    return null;
+  }
+}
+
+const addContact = async (body) => {
+  try {
+    const { name, email, phone } = body;
+
+
+    const allContacts = await listContacts();
+
+    const duplicateTrue = allContacts.some(
+      el => el.email.toLowerCase().trim() === email.toLowerCase().trim() ||
+        el.phone.toLowerCase().trim() === phone.toLowerCase().trim()
+    );
+    if (duplicateTrue) {
+      return null;
+    }
+
+    const id = uuidv1();
+    const newContact = { id, name, email, phone };
+
+    allContacts.push(newContact);
+    fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return newContact || null;
+
+  } catch (error) {
+    console.error(err);
+    return null;
+  }
+}
+
+const updateContact = async (id, body) => {
+  try {
+    const allContacts = await listContacts();
+    const indexEl = allContacts.findIndex(el => el.id === id);
+
+    if (indexEl === -1) return null;
+
+    const { name, email, phone } = body;
+
+    const duplicateTrue = allContacts.some(
+      el => el.email.toLowerCase().trim() === email.toLowerCase().trim() ||
+        el.phone.toLowerCase().trim() === phone.toLowerCase().trim()
+    );
+    if (duplicateTrue) {
+      return null;
+    }
+
+    const newContact = { id, name, email, phone };
+
+    allContacts.splice(indexEl, 1, newContact);
+    fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return newContact;
+
+  } catch (error) {
+    console.error(err);
+    return null;
+  }
+}
+
+export default {
   listContacts,
   getContactById,
   removeContact,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,127 @@
-const express = require('express')
+import { Router } from 'express'
+import contacts from '../../models/contacts.js'
 
-const router = express.Router()
+import Joi from 'joi'
+import JoiPhoneNumber from 'joi-phone-number';
+
+const myJoi = Joi.extend(JoiPhoneNumber);
+
+const schemaValidation = myJoi.object({
+  name: Joi.string().min(3).max(255).required(),
+  email: Joi.string().email().required(),
+  phone: myJoi.string().phoneNumber().required(),
+});
+
+const router = Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contactList = await contacts.listContacts();
+
+  if (!contactList) return res.status(404).json({
+    status: 'Not Found',
+    code: 404,
+    message: 'Not found',
+    data: contactList,
+  })
+
+  res.json({
+    status: "OK",
+    code: 200,
+    data: contactList,
+  });
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const contactId = req.params.contactId;
+  const currentContact = await contacts.getContactById(contactId);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  if (!currentContact) return res.status(404).json({
+    status: 'Not Found',
+    code: 404,
+    message: 'Not found',
+    data: currentContact,
+  })
+
+  res.json({
+    status: "OK",
+    code: 200,
+    data: currentContact,
+  });
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contactId = req.params.contactId;
+  const currentContact = await contacts.removeContact(contactId);
+
+  if (!currentContact) return res.status(404).json({
+    status: 'Not Found',
+    code: 404,
+    message: 'Not found',
+    data: currentContact,
+  })
+
+  res.json({
+    status: "OK",
+    code: 200,
+    data: currentContact,
+  });
+})
+
+router.post('/', async (req, res, next) => { 
+  const { name, email, phone } = req.body;
+
+  const { error } = schemaValidation.validate(req.body)
+
+  if (error) return res.status(400).json({
+    status: 'Not Found',
+    code: 404,
+    message: 'missing required name field',
+    data: { name, email, phone },
+  });
+
+  const currentContact = await contacts.addContact({ name, email, phone });
+
+  if (!currentContact) return res.status(404).json({
+    status: 'Not Found',
+    code: 404,
+    message: 'Not found',
+    data: currentContact,
+  })
+  
+  res.json({
+    status: "Created",
+    code: 201,
+    data: currentContact,
+  });
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+  const { name, email, phone } = req.body;
+
+  const { error } = schemaValidation.validate(req.body)
+
+  if (error) return res.status(400).json({
+    status: 'Not Found',
+    code: 404,
+    message: 'missing fields',
+    data: { name, email, phone },
+  });
+
+  const currentContact = await contacts.updateContact(contactId,{ name, email, phone });
+
+  if (!currentContact) return res.status(404).json({
+    status: 'Not Found',
+    code: 404,
+    message: 'Not found',
+    data: currentContact,
+  })
+  
+  res.json({
+    status: "OK",
+    code: 200,
+    data: currentContact,
+  });
 })
 
-module.exports = router
+export default router
